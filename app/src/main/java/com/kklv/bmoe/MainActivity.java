0fa861e86250334cbef6ccf684491027bf14d3c0
lvzhendong <lvzhendong1993@gmail.com>
2016-06-01 16:56:06 +0800

    workspace

--- app/src/main/java/com/kklv/bmoe/MainActivity.java
+++ app/src/main/java/com/kklv/bmoe/MainActivity.java
@@ -3,28 +3,12 @@ package com.kklv.bmoe;
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.kklv.bmoe.data.HttpUtil;
-
 public class MainActivity extends Activity {
-    HttpUtil mHttpUtil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mHttpUtil=new HttpUtil(this);
-        test();
     }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        test();
     }
-
-    private void test() {
-        mHttpUtil.get();
-    }
-
-
 }
