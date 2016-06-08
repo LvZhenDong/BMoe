689fb1e087bc19037f942c1ab5be16ec4e7ae1c1
lvzhendong <lvzhendong1993@gmail.com>
2016-06-08 09:48:11 +0800

    UI:基本架构

--- app/src/main/java/com/kklv/bmoe/MainActivity.java
+++ app/src/main/java/com/kklv/bmoe/MainActivity.java
@@ -2,12 +2,23 @@ package com.kklv.bmoe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.kklv.bmoe.chart.Chart;
 
-public class MainActivity extends Activity{
+public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
     private LineChart mLineChart;
     private Chart mChart;
     @Override
@@ -21,12 +32,29 @@ public class MainActivity extends Activity{
     }
 
     private void bindId() {
-        mLineChart = (LineChart) findViewById(R.id.lineChart);
+//        mLineChart = (LineChart) findViewById(R.id.lineChart);
+        mDrawerLayout= (DrawerLayout) findViewById(R.id.drawer_layout);
+        mNavigationView= (NavigationView) findViewById(R.id.nav_view);
     }
 
     private void initView() {
-        mChart=new Chart(this,mLineChart);
-        mChart.showData();
+//        mChart=new Chart(this,mLineChart);
+//        mChart.showData();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ActionBar ab=getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
