a3e36496208f6033292a651122422e0f5f1b0509
lvzhendong <lvzhendong1993@gmail.com>
2016-06-08 10:52:24 +0800

    UI:加入Fragment

--- app/src/main/java/com/kklv/bmoe/MainActivity.java
+++ app/src/main/java/com/kklv/bmoe/MainActivity.java
@@ -3,24 +3,37 @@ package com.kklv.bmoe;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.kklv.bmoe.chart.Chart;
+import com.kklv.bmoe.fragment.CampFragment;
+import com.kklv.bmoe.fragment.LineChartFragment;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     private DrawerLayout mDrawerLayout;
     private NavigationView mNavigationView;
-    private LineChart mLineChart;
-    private Chart mChart;
+
+    private FragmentManager mFragmentManager;
+//    private FragmentTransaction mFragmentTransaction;
+    private LineChartFragment mLineChartFragment;
+    private CampFragment mCampFragment;
+//    private LineChart mLineChart;
+//    private Chart mChart;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,8 +46,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void bindId() {
 //        mLineChart = (LineChart) findViewById(R.id.lineChart);
-        mDrawerLayout= (DrawerLayout) findViewById(R.id.drawer_layout);
-        mNavigationView= (NavigationView) findViewById(R.id.nav_view);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
     }
 
     private void initView() {
@@ -43,14 +56,50 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final ActionBar ab=getSupportActionBar();
+        final ActionBar ab = getSupportActionBar();
         ab.setHomeAsUpIndicator(R.drawable.ic_menu);
         ab.setDisplayHomeAsUpEnabled(true);
+
+        setupDrawerContent(mNavigationView);
+
+        mFragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = mFragmentManager.beginTransaction();
+
+        mCampFragment=new CampFragment();
+        mLineChartFragment=new LineChartFragment();
+        transaction.replace(R.id.fl_fragment,mLineChartFragment);
+        transaction.commit();
+    }
+
+    private void setupDrawerContent(NavigationView navigationView){
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                if (!item.isChecked()){
+                    switch (item.getItemId()){
+                        case R.id.nav_chat:
+                            FragmentTransaction transaction = mFragmentManager.beginTransaction();
+                            transaction.add(R.id.fl_fragment,mCampFragment);
+
+                            transaction.commit();
+                            break;
+                    }
+                }
+                item.setChecked(true);
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actions_menu, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case android.R.id.home:
                 mDrawerLayout.openDrawer(GravityCompat.START);
                 return true;
