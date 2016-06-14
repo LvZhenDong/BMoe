c40414e1df92f256681da666fbfebdc0088aaff7
lvzhendong <lvzhendong1993@gmail.com>
2016-06-14 09:51:57 +0800

    解决HroizontalScrollView白边和显示错误问题

--- app/src/main/java/com/kklv/bmoe/activity/CampRankActivity.java
+++ app/src/main/java/com/kklv/bmoe/activity/CampRankActivity.java
@@ -4,7 +4,7 @@ package com.kklv.bmoe.activity;
  *角色数据表格
  * @author LvZhenDong
  * @email lvzhendong1993@gmail.com
- * created at 2016/6/13 16:18
+ * created at 2016/6/13 17:43
  */
 public class CampRankActivity extends BaseActivity {
     
