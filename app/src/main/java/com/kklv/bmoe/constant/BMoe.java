d1356fdb2cacf99b01793af0f929c6e0817ade5c
lvzhendong <lvzhendong1993@gmail.com>
2016-08-19 16:32:56 +0800

    commit

---
+++ app/src/main/java/com/kklv/bmoe/constant/BMoe.java
@@ -0,0 +1,36 @@
+package com.kklv.bmoe.constant;
+
+/**
+ *
+ * 
+ * @author LvZhenDong
+ * created at 2016/8/19 16:32
+ */
+public class BMoe {
+    /**
+     * 2015年10月有数据的日期
+     */
+    public static final int[] SELECTED_2015_10_DAYS = {31};
+    /**
+     * 2015年11月有数据的日期
+     */
+    public static final int[] SELECTED_2015_11_DAYS = {1,
+            2, 3, 4, 5, 6, 7, 8,
+            9, 10, 11, 12, 13,14, 15,
+            16, 17, 18, 19, 20, 21, 22,
+            23, 24, 25, 26, 27, 28, 29,
+            30};
+    /**
+     * 2015年12月有数据的日期
+     */
+    public static final int[] SELECTED_2015_12_DAYS = {1, 2, 3, 4, 5,
+            7, 8, 9, 10, 12, 13,
+            14, 15, 16, 17, 18, 19, 20,
+            21, 22, 23, 24, 25, 27,
+            28, 29, 30, 31};
+    /**
+     * 2016年1月有数据的日期
+     */
+    public static final int[] SELECTED_2016_01_DAYS = {1, 2, 3, 9};
+
+}
